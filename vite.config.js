9a47d7918c61996160ae41287d905e93d2be5e56
HP-Tur <tur.1i@HOTMAIL.COM>
2024-07-19 23:41:10 +0300

    Remove .env file from repository and update .gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: 'august.test', 
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
